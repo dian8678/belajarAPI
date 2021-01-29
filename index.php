7f8e4a64a010ee27dd8da6478d6e3a539e3647c1
DESKTOP-47HMCH6\Lenovo <dianmarta136@gmal.com>
2021-01-30 01:31:37 +0700

    second commit

--- index.php
+++ index.php
@@ -39,5 +39,64 @@ $app ->get('/listcatatan', function() use($app, $db){
        echo json_encode($res);
    });
 
+   $app->post('/editcatatan/{id}', function($request, $response, $args) use($app, $db){
+    $q = $db->tb_catatan()->where('id', $args);
+    if($q->fetch()){
+        $param = $request->getParams();
+        $r = $q->update($param);
+        echo json_encode(array(
+            "error" => false,
+            "message" => "Catatan berhasil diupdate"));
+    }else{
+        echo json_encode(array(
+            "error" => true,
+            "message" => "Catatan tersebut tidak ada"));
+    }
+});
+
+$app->post('/deletecatatan/{id}', function($request, $response, $args) use($app, $db){
+    $q = $db->tb_catatan()->where('id', $args);
+    if($q->fetch()){
+        $param = array("status" => 1);
+        $r = $q->update($param);
+        echo json_encode(array(
+            "error" => false,
+            "message" => "Catatan berhasil di delete"));
+    }else{
+        echo json_encode(array(
+            "error" => true,
+            "message" => "Catatan tersebut tidak ada"));
+    }
+});
+$app->post('/restorecatatan/{id}', function($request, $response, $args) use($app, $db){
+    $q = $db->tb_catatan()->where('id', $args);
+    if($q->fetch()){
+        $param = array("status" => 0);
+        $r = $q->update($param);
+        echo json_encode(array(
+            "error" => false,
+            "message" => "Catatan berhasil di restore"));
+    }else{
+        echo json_encode(array(
+            "error" => true,
+            "message" => "Catatan tersebut tidak ada"));
+    }
+});
+$app ->get('/logcatatan', function() use($app, $db){
+ $res["error"] = false;
+    $res["message"] = "Berhasil mendapatkan log Catatan";
+    foreach($db->v_catatan_log() as $data){
+        $res['data'][] = array(
+            'id' => $data['id'],
+            'id_catatan' => $data['id_catatan'],
+            'method' => $data['method'],
+            'judul' => $data['judul'],
+            'catatan' => $data['catatan'],
+            'update_at' => $data['update_at']
+            );
+    }
+    echo json_encode($res);
+});
+
 
 $app->run();
